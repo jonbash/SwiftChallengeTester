2f67c43f93c7e81773eb32b1d9a609e2d91201da
Jon Bash <jonbash@protonmail.com>
2020-09-06 17:41:02 -0700

    basically rewrite the whole thing

--- Sources/SwiftChallengeTester/SwiftChallengeTester.swift
+++ Sources/SwiftChallengeTester/SwiftChallengeTester.swift
@@ -1,102 +1,205 @@
-public struct CodeChallengeTestCases<Input, Output> {
-    public var title: String?
-    public var expected: KeyValuePairs<Input, [Output]>
-    public var solution: (Input) -> Output
+import Foundation
 
-    internal var failures: [Failure]?
+public struct CodeChallengeTestCases<Input: Hashable, Output> {
+    public var title: String
+    public var expected: [Input: [Output]]
+    public var solution: (Input) -> Output
 
     public init(
         title: String? = nil,
-        expected: KeyValuePairs<Input, [Output]> = [:],
-        solution: @escaping (Input) -> Output)
-    {
-        self.title = title
-        self.expected = expected
+        expected: KeyValuePairs<Input, Output> = [:],
+        solution: @escaping (Input) -> Output
+    ) {
+        self.title = title ?? "\(Input.self) -> \(Output.self)"
+        self.expected = [:]
+        for (i, o) in expected {
+            self.expected[i, default: []].append(o)
+        }
         self.solution = solution
     }
-}
 
-public extension CodeChallengeTestCases {
-    struct Failure {
-        public let input: Input
-        public let expectedOutput: [Output]
-        public let actualOutput: Output
-
-        public func print() {
-            Swift.print("Input:        \t\(input)\n"
-                            +  "Expected:     \t\(expectedOutput)\n"
-                            +  "Actual output:\t\(actualOutput)"
-            )
-        }
+    public mutating func append(_ input: Input, expecting output: Output) {
+        self.expected[input, default: []].append(output)
     }
 
-    var isEmpty: Bool { expected.isEmpty }
-
-    func evaluate(_ outputEqualsExpected: (Output, Output) -> Bool) -> Self {
-        var new = self
-        new.failures = expected.compactMap { ioPair -> Failure? in
-            let o = output(for: ioPair.key)
-            let es = ioPair.value
-
-            for e in es {
-                if outputEqualsExpected(o, e) {
-                    return nil
-                }
-            }
-            return Failure(input: ioPair.key, expectedOutput: es, actualOutput: o)
+    public mutating func append(_ pairs: KeyValuePairs<Input, Output>) {
+        for (i, o) in pairs {
+            self.expected[i, default: []].append(o)
         }
-        return new
-    }
-
-    func printFailures() {
-        guard let evaluatedFailures = failures else {
-            return print("Output must be evaluated before printing.")
-        }
-
-        let titleText = title ?? "\(Input.self) -> \(Output.self)"
-
-        if evaluatedFailures.isEmpty {
-            print("All tests passed for '\(titleText)'!\n")
-            return
-        }
-
-        print("Tests failed for '\(titleText)':")
-        for f in evaluatedFailures {
-            printEvaluation(for: f.input,
-                            expected: f.expectedOutput,
-                            actual: f.actualOutput)
-        }
-        print("\n----------------\n")
-    }
-
-    func printEvaluations() {
-        expected.forEach { ioPair in
-            printEvaluation(for: ioPair.key,
-                            expected: ioPair.value,
-                            actual: solution(ioPair.key))
-        }
-    }
-}
-
-public extension CodeChallengeTestCases where Output: Equatable {
-    func evaluate() -> Self {
-        evaluate { $0 == $1 }
     }
 }
 
 extension CodeChallengeTestCases {
-    private func output(for input: Input) -> Output {
-        solution(input)
+    public var isEmpty: Bool { expected.isEmpty }
+
+    @discardableResult
+    public func evaluate(
+        _ outputEqualsExpected: (Output, Output) -> Bool,
+        evaluationCount: Int = 1
+    ) -> CodeChallengeEvaluation<Input, Output> {
+        CodeChallengeEvaluation(
+            title: title,
+            results: expected.map { (input, possibleOutputs) in
+                let start = CFAbsoluteTimeGetCurrent()
+                let actualOutput = solution(input)
+                let timeAllotted = CFAbsoluteTimeGetCurrent() - start
+
+                let success = possibleOutputs.reduce(false) { didSucceed, expectedOutput in
+                    outputEqualsExpected(actualOutput, expectedOutput) || didSucceed
+                }
+                if success {
+                    return .success(
+                        CodeChallengeSuccess(
+                            input: input,
+                            output: actualOutput,
+                            time: timeAllotted))
+                } else {
+                    return .failure(
+                        CodeChallengeFailure(
+                            input: input,
+                            expected: possibleOutputs,
+                            actual: actualOutput,
+                            time: timeAllotted))
+                }
+            }
+        )
     }
 
-    private func printEvaluation(
-        for input: Input,
-        expected: [Output],
-        actual: Output)
-    {
-        print("Input:        \t\(input)\n"
-           +  "Expected:     \t\(expected)\n"
-           +  "Actual output:\t\(actual)"
+    public func printFailures(evaluating outputEqualsExpected: (Output, Output) -> Bool) {
+        let failures = evaluate(outputEqualsExpected).failures
+
+        if failures.isEmpty {
+            print("All tests passed for '\(title)'!\n")
+            return
+        }
+
+        print("Tests failed for '\(title)':")
+        for f in failures {
+            f.print()
+        }
+        print("\n----------------\n")
+    }
+}
+
+public extension CodeChallengeTestCases where Output: Equatable {
+    func evaluate() -> CodeChallengeEvaluation<Input, Output> {
+        evaluate { $0 == $1 }
+    }
+
+    func printFailures() {
+        printFailures(evaluating: { $0 == $1 })
+    }
+}
+
+// MARK: - Results
+
+public struct CodeChallengeSuccess<Input, Output> {
+    public let input: Input
+    public let output: Output
+    public let time: Double
+
+    public func print() {
+        Swift.print("Input:        \t\(input)\n"
+                        +  "Output:       \t\(output)\n"
+                        +  "Time to solve:\t\(time)"
         )
+    }
+}
+
+public struct CodeChallengeFailure<Input, Output>: Error {
+    public let input: Input
+    public let expected: [Output]
+    public let actual: Output
+    public let time: Double
+
+    public func print() {
+        Swift.print("Input:        \t\(input)\n"
+                        +  "Expected:     \t\(expected)\n"
+                        +  "Actual output:\t\(actual)\n"
+                        +  "Time to solve:\t\(time)"
+        )
+    }
+}
+
+public typealias CodeChallengeResult<I, O> = Result<CodeChallengeSuccess<I, O>, CodeChallengeFailure<I, O>>
+
+public struct CodeChallengeEvaluation<I, O> {
+    public let title: String
+    public let results: [CodeChallengeResult<I, O>]
+
+    public var allSuccess: Bool {
+        for result in results {
+            if case .failure = result {
+                return false
+            }
+        }
+        return true
+    }
+
+    public var failures: [CodeChallengeFailure<I, O>] {
+        results.compactMap {
+            if case .failure(let fail) = $0 {
+                return fail
+            } else { return nil }
+        }
+    }
+
+    public var successes: [CodeChallengeSuccess<I, O>] {
+        results.compactMap {
+            if case .success(let success) = $0 {
+                return success
+            } else { return nil }
+        }
+    }
+
+    public var totalTime: Double {
+        results.lazy
+            .map { $0.time() }
+            .reduce(0, +)
+    }
+
+    public func printSuccesses() {
+        let scs = successes
+        if scs.isEmpty {
+            return Swift.print("All tests failed for '\(title)'.\n")
+        }
+        Swift.print("Tests suceeeded for '\(title)':")
+        for s in scs { s.print() }
+        Swift.print("\n----------------\n")
+    }
+
+    public func printFailures() {
+        if allSuccess {
+            return Swift.print("All tests passed for '\(title)'!\n")
+        }
+
+        Swift.print("Tests failed for '\(title)':")
+        for f in failures { f.print() }
+        Swift.print("\n----------------\n")
+    }
+
+    public func print() {
+        Swift.print("Test results for '\(title)':")
+        for r in results {
+            switch r {
+            case .success(let s):
+                s.print()
+            case .failure(let f):
+                f.print()
+            }
+        }
+        Swift.print("\n----------------\n")
+    }
+}
+
+
+extension Result {
+    public func time<I, O>() -> Double where Success == CodeChallengeSuccess<I, O>, Failure == CodeChallengeFailure<I, O> {
+        switch self {
+        case .success(let success):
+            return success.time
+        case .failure(let failure):
+            return failure.time
+        }
     }
 }
